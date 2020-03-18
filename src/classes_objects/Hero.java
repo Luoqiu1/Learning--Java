d4b9f8f88eae9f0eb09c82f14232798c72cb337b
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2020-03-18 17:05:14 +0800

    new 代表创建了一个对象，也仅仅只是创建了一个对象，无法访问他
    Hero h=new Hero();
    右侧是new了一个Hero对象，而左侧意思是另h指向了这个刚刚创建的Hero类型的对象！（引用）有点指针内味。所以代码验证了多个引用，引用一个对象。

---
+++ src/classes_objects/Hero.java
@@ -0,0 +1,22 @@
+package classes_objects;
+
+public class Hero {
+	String name; //姓名
+    float hp; //血量     
+    float armor; //护甲
+    int moveSpeed; //移动速度      
+    public static void main(String[] args) {
+        //创建一个对象
+        new Hero();        
+        //使用一个引用来指向这个对象
+        Hero h = new Hero();     
+        h.hp=3.333f;
+        Hero h2=h;
+        Hero h3=h2;
+        System.out.println(h3.hp);
+        h3.hp=2.222f;
+        System.out.println(h2.hp);
+        System.out.println(h3.hp);
+        System.out.println(h.hp);
+    }  
+}
