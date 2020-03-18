6c5ea62f106bef38400267b182a39d14626af539
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2020-03-18 22:51:09 +0800

    深入理解传参..
    变量有两种类型 基本类型 和类类型
    参数也是变量，所以传参分为
    基本类型传参，= 表示赋值的意思
    类类型传参，= 表示指向的意思

--- src/classes_objects/Hero.java
+++ src/classes_objects/Hero.java
@@ -4,12 +4,24 @@ public class Hero {
 	String name; //姓名
     float hp; //血量     
     float armor; //护甲
-    int moveSpeed; //移动速度      
-//    public static void main(String[] args) {
+    int moveSpeed; //移动速度     
+    public Hero() {
+    }
+    public Hero(String name,float hp) {
+    	this.name=name;
+    	this.hp=hp;
+    }
+    public Hero(String name,float hp,float armor,int moveSpeed) {
+    	this(name,hp);
+    	this.armor=armor;
+    	this.moveSpeed=moveSpeed;
+    }
+    
+    public static void main(String[] args) {
 //        //创建一个对象
 //        new Hero();        
 //        //使用一个引用来指向这个对象
-//        Hero h = new Hero();     
+        Hero h = new Hero();     
 //        h.hp=3.333f;
 //        Hero h2=h;
 //        Hero h3=h2;
@@ -18,5 +30,7 @@ public class Hero {
 //        System.out.println(h2.hp);
 //        System.out.println(h3.hp);
 //        System.out.println(h.hp);
-//    }  
+        
+        Hero h5=new Hero("牛牛",555,30,300);
+    }  
 }
