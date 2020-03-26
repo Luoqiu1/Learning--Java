d1baf4bab74391dcb3a1214728b8edb918dbdbc6
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2020-03-26 15:06:52 +0800

    java中对String及char[]字符数组的一些理解
    java中String好像不适合来对具体字符操作，比如索引、更改具体某个索引上的字符等。若要进行字符操作，可以先讲String转化为字符数组的形式，直接在具体位置上进行更改，最后再将字符数组改回String即可！

--- src/classes_objects/Text.java
+++ src/classes_objects/Text.java
@@ -2,20 +2,28 @@ package classes_objects;
 
 public class Text {
 	public static void main(String[] args) {
-		String s1=new String("let there be light");
-		String str[]=s1.split(" ");
-		for(int i=0;i<str.length;++i) {
-//			System.out.println(str[i]);
-//			str[i].replaceFirst("l","L");
-//			System.out.println(str[i]);
-//			System.out.println(String.valueOf(Character.toUpperCase(str[i].charAt(0))));
-			//注意！！要str[i]=str[i].replace......
-			//因为replace方法不会直接替换str的值，只是会返回一个替换后的新字符串！
-			str[i]=str[i].replaceFirst(String.valueOf(str[i].charAt(0)),String.valueOf(Character.toUpperCase(str[i].charAt(0))));
+		String s="lengendary";
+		char s1[]=s.toCharArray();
+			for (int i = 0; i < s1.length; i++) {
+		//	System.out.println(s1);
+//			char x1=s1.charAt(i);
+//			char x2=Character.toUpperCase(s1.charAt(i));
+//			char x3=Character.toLowerCase(s1.charAt(i));
+			char x1=s1[i];
+			char x2=Character.toUpperCase(x1);
+			char x3=Character.toLowerCase(x1);
+			if(i%2==0) {
+		//		System.out.printf("偶数 %d %c %c%n",i,x1,x2);
+			//	s1=s1.replaceFirst(String.valueOf(x1),String.valueOf(x2));
+				s1[i]=x2;
+			}
+			else {
+		//		System.out.printf("奇数 %d %c %c%n",i,x1,x3);
+			//	s1=s1.replaceFirst(String.valueOf(x1),String.valueOf(x3));
+				s1[i]=x3;
+			}
+		//	System.out.println();
 		}
-		String s2="";
-		for(int i=0;i<str.length-1;++i)s2+=str[i]+" ";
-		s2+=str[str.length-1];
-		System.out.println(s2);
+		System.out.println(String.valueOf(s1));
 	}
 }
