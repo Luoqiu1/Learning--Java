9cead1d06b29c135e1e4df4a6e9ee2117b8b7227
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2020-03-27 00:42:58 +0800

    MyStringBuffer类实现！
    抓住capacity,length来操作！
    注意输出的时候都要以length来界定范围！
    数组增长消减的时候注意要改动capacity和length！

---
+++ src/classes_objects/MyStringBuffer.java
@@ -0,0 +1,69 @@
+package classes_objects;
+
+import java.util.Arrays;
+
+public class MyStringBuffer implements IStringBuffer {
+//	private String s;
+	private int length=0;
+	private int capacity=16;
+	private char value[];
+	public MyStringBuffer() {
+//		s="";
+		value=new char[capacity];
+	}
+	public MyStringBuffer(String s) {
+//		this.s=s;
+		if(s.length()>capacity) {
+			capacity=(int)((capacity+s.length())*1.5);
+		}
+		value=new char[capacity];
+		System.arraycopy(s.toCharArray(), 0, value, 0, s.length());
+		length=s.length();
+	}
+	public void value() {
+	//	System.out.println(String.valueOf(value));
+		
+		for(int i=0;i<length;++i)System.out.print(value[i]);
+		System.out.println();
+	}
+	public void append(String str) {
+		insert(length,str);
+	}
+	public void append(char c) {
+		append(String.valueOf(c));
+	}
+	public void insert(int pos,char b) {
+		insert(pos,String.valueOf(b));
+	}
+	public void insert(int pos,String b) {
+		if(pos<0||pos>length||null==b)return;
+		if(b.length()+length>capacity) {
+			capacity=(int)((b.length()+length)*1.5);
+			char str[]=new char[capacity];
+			str=Arrays.copyOf(value, length);
+			value=str;
+		}		
+		System.arraycopy(value,pos,value,pos+b.length(),length-pos);
+		System.arraycopy(b.toCharArray(), 0, value, pos, b.length());
+		length+=b.length();
+	}
+	public void delete(int start) {
+		delete(start,length);
+	}
+	public void delete(int start,int end) {
+		if(start<0||start>length||end<0||end>length||start>=end)return;
+		System.arraycopy(value, end, value, start, length-end);
+		length-=end-start;
+	}
+	public void reverse() {
+		for(int i=0;i<length/2;++i) {
+			char c=value[i];
+			value[i]=value[length-i-1];
+			value[length-i-1]=c;
+		}
+	}
+	public int length() {
+		return length;
+	}
+	
+}
--- src/classes_objects/Text.java
+++ src/classes_objects/Text.java
@@ -1,29 +1,30 @@
 package classes_objects;
 
-public class Text {
+public class Text extends MyStringBuffer {
 	public static void main(String[] args) {
-		String s="lengendary";
-		char s1[]=s.toCharArray();
-			for (int i = 0; i < s1.length; i++) {
-		//	System.out.println(s1);
-//			char x1=s1.charAt(i);
-//			char x2=Character.toUpperCase(s1.charAt(i));
-//			char x3=Character.toLowerCase(s1.charAt(i));
-			char x1=s1[i];
-			char x2=Character.toUpperCase(x1);
-			char x3=Character.toLowerCase(x1);
-			if(i%2==0) {
-		//		System.out.printf("Å¼Êý %d %c %c%n",i,x1,x2);
-			//	s1=s1.replaceFirst(String.valueOf(x1),String.valueOf(x2));
-				s1[i]=x2;
-			}
-			else {
-		//		System.out.printf("ÆæÊý %d %c %c%n",i,x1,x3);
-			//	s1=s1.replaceFirst(String.valueOf(x1),String.valueOf(x3));
-				s1[i]=x3;
-			}
-		//	System.out.println();
-		}
-		System.out.println(String.valueOf(s1));
+//		String s="hello!";System.out.println(s);
+//		String s1=s.substring(0,5);
+//		String s2=s.substring(5,6);
+//		s=s2+s2;
+//		
+//		System.out.println(s1);System.out.println(s2);System.out.println(s);
+//		System.out.println();
+		MyStringBuffer s0=new MyStringBuffer
+				("hello!");
+		s0.append('A');
+		s0.value();
+		s0.append("³æ");
+		s0.value();
+		s0.delete(6);
+		s0.value();
+		s0.delete(5, 6);
+		s0.value();
+		System.out.println(s0.length());
+		s0.reverse();
+		s0.value();
+		s0.insert(5, '!');
+		s0.value();
+		s0.insert(6, "GoGo!");
+		s0.value();
 	}
 }
