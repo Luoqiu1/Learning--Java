8a5962960ee495b03d614f73e5b7b8b08d3c17e4
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2020-03-30 16:37:53 +0800

    文件拆分、合并：通过File相关方法进行！
    打开文件（文件的输入输出流）的时候一定要记得关闭！
    进行read、write等操作的时候。
    还有，当想持续写入的时候，不要在还没写完的时候就close掉！应该用flush（没有有时候也不会错，最好是要及时刷新写入，避免close前没有写完！），及时将缓冲区的东西写入，最后再close！
    加深对close操作的理解！

--- src/classes_objects/TestFile.java
+++ src/classes_objects/TestFile.java
@@ -5,27 +5,82 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class TestFile {
+	static File file=null;
+	static boolean flag=false;
+	public static void FileList(File f) {
+		if(flag)return;
+		if(f.isDirectory()) {
+			File f0[]=f.listFiles();
+			for(File x:f0)FileList(x);
+		}
+		if(f.isFile()) {
+			if(f.length()>100*1000&&f.length()<Integer.MAX_VALUE) {
+				file=f;
+				flag=true;return;
+			}
+		}
+	}
 	public static void main(String[] args) {
-		
+		File f=new File("C:\\游戏");
+		FileList(f);
+		System.out.printf("找到了该文件，其绝对路径为：%s%n",file.getAbsolutePath());
+//		File f0=new File(file.getAbsolutePath()+"-0");
+//		File f1=new File(file.getAbsolutePath()+"-1");
+//		File f2=new File(file.getAbsolutePath()+"-2");
+//		File f3=new File(file.getAbsolutePath()+"-3");
+		int n=(int)file.length()/4;
+		int m=(int)file.length()%4;
 		try {
-			File f=new File("c:/xyz/abc/def/lol2.txt");
-			f.mkdir();
-//			if(!f.getParentFile().exists()) {
-//				f.getParentFile().mkdirs();
-//				f.mkdirs();
-//			}
-			FileOutputStream fos=new FileOutputStream(f);
-//			byte a[]= {41,23};
-//			fos.write(a);
-//			fos.close();
-		
+			byte a[]=new byte[(int)file.length()];
+			FileInputStream fis=new FileInputStream(file);
+			fis.read(a);
+			fis.close();
+			for(int i=0;i<4;++i) {
+				String child=file.getName()+"-"+i;
+			//	File fx=new File(file.getAbsolutePath()+"-"+i);
+				File fx=new File(file.getParent(),child);
+				FileOutputStream fos=new FileOutputStream(fx);
+				byte b[]=new byte[n+m];
+				if(i!=3) {
+				System.arraycopy(a, i*n, b, 0, n);
+				}
+				else {
+				System.arraycopy(a, i*n, b, 0, n+m);
+				}
+				fos.write(b);
+				fos.close();
+			}
+			byte src[]=new byte[(int)file.length()];
+			File newf=new File(file.getParent()+"/副本"+file.getName());
+			FileOutputStream fos2=new FileOutputStream(newf);	
+			for(int i=0;i<4;++i) {
+				String name=file.getAbsolutePath()+"-"+i;
+				File fx=new File(name);
+				byte b[]=new byte[n+m];
+				FileInputStream fis2=new FileInputStream(fx);
+				fis2.read(b);
+				fis2.close();
+				fos2.write(b);
+			//	fos2.flush();
+//				if(i!=3) {
+//					System.arraycopy(b, 0, src, i*n, n);
+//				}
+//				else {
+//					System.arraycopy(b, 0, src, i*n, n+m);
+//				}
+			}
+			fos2.close();
+//			File newf=new File(file.getParent()+"/副本"+file.getName());
+//			FileOutputStream fos2=new FileOutputStream(newf);
+//			fos2.write(src);
+//			fos2.close();
 		}
 		catch(IOException e) {
 			e.printStackTrace();
 		}
-		
 		
 	}
 }
