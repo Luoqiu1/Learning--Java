0979ce1d6855fdeb24e80bd76d14cfbf2fc4e301
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2020-03-30 14:14:39 +0800

    方法mkdir/mkdirs及getParentFile文件处理再理解！
    File f=new File("c:/xyz/abc/def/lol2.txt");
    f.mkdir();
    如果存在c:/xyz/abc/def，则该方法可以顺利创建lol2.txt文件！
    但若c:/xyz/abc/def不存在，该方法返回创建目标文件失败！
    但若是f.mkdirs();
    就算父目录不存在，也会为了能够在指定目录下创建文件lol2.txt，而事先创建前面的全部父目录！同时也创建了一个lol2.txt目录！而不是lol2的一个txt文件！
    所以经常搭配f.getParentFile.mkdirs;来使用创建父目录！

--- src/classes_objects/TestFile.java
+++ src/classes_objects/TestFile.java
@@ -1,48 +1,31 @@
 package classes_objects;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
-  
+
 public class TestFile {
-	public static File Maxfile=null;
-	public static File Minfile=null;
-	public static long Maxsize=0;
-	public static long Minsize=Integer.MAX_VALUE;
-	
-	public static void jg(File f) {
-		if(f.length()>Maxsize) {
-			Maxfile=f;
-			Maxsize=f.length();
-		}
-		if(f.length()<Minsize&&f.length()>0) {
-			Minfile=f;
-			Minsize=f.length();
-		}
-	}
-	public static void dfs(File f) {
-		if(f.isDirectory()) {
-			File f0[]=f.listFiles();
-//			for(int i=0;i<f0.length;++i) {
-//				File x=f0[0].getAbsoluteFile();
-//				dfs(x);
+	public static void main(String[] args) {
+		
+		try {
+			File f=new File("c:/xyz/abc/def/lol2.txt");
+			f.mkdir();
+//			if(!f.getParentFile().exists()) {
+//				f.getParentFile().mkdirs();
+//				f.mkdirs();
 //			}
-			for(File x:f0)dfs(x);
+			FileOutputStream fos=new FileOutputStream(f);
+//			byte a[]= {41,23};
+//			fos.write(a);
+//			fos.close();
+		
 		}
-		if(f.isFile()) jg(f);
-		if(f==null);
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		
 	}
-    public static void main(String[] args) {
-        File f=new File("C:\\游戏");
-//        File f0[]=f.listFiles();
-////        for(File x:f1) {
-////        	dfs(x);
-////        }
-//        for(int i=0;i<f0.length;++i) {
-//			File x=new File(f0[0].getAbsolutePath());
-//			dfs(x);
-//		}
-        dfs(f);
-        System.out.printf("最大的文件是%s，其大小是%,d字节%n",Maxfile.getAbsolutePath(),Maxfile.length());
-        System.out.printf("最小的文件是%s，其大小是%,d字节%n",Minfile.getAbsolutePath(),Minfile.length());
-    }
 }
