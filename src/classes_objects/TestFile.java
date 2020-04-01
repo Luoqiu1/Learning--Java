a489b9c1e488f89d07c69cfeeef0edffd4706b78
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2020-04-01 11:20:52 +0800

    注意创建输入输出流的顺序！：在对一个文件修改的时候
    创建非追加（？应该有这个东西）输出流时，//
    因为Java输出流默认的是覆盖的方式，如果需要追加方式写入文件，在后面加一个参数true，即File OutputStream(file, true) 。
    //
    
    注释处为网上找的说法。还没学到 先这样看
    
    （注意：要在通过BuffedReader 读取完数据后，才能建立Printwriter，因为创建输出流的时候，会把目标文件内容清空）

--- src/classes_objects/TestFile.java
+++ src/classes_objects/TestFile.java
@@ -27,22 +27,16 @@ public class TestFile {
 		File f=new File("C:\\游戏");
 		FileList(f);
 		System.out.printf("找到了该文件，其绝对路径为：%s%n",file.getAbsolutePath());
-//		File f0=new File(file.getAbsolutePath()+"-0");
-//		File f1=new File(file.getAbsolutePath()+"-1");
-//		File f2=new File(file.getAbsolutePath()+"-2");
-//		File f3=new File(file.getAbsolutePath()+"-3");
 		int n=(int)file.length()/4;
 		int m=(int)file.length()%4;
 		FileInputStream fis=null;
 		try {
 			byte a[]=new byte[(int)file.length()];
-//			FileInputStream fis=new FileInputStream(file);
 			fis=new FileInputStream(file);
 			fis.read(a);
 			fis.close();
 			for(int i=0;i<4;++i) {
 				String child=file.getName()+"-"+i;
-			//	File fx=new File(file.getAbsolutePath()+"-"+i);
 				File fx=new File(file.getParent(),child);
 				FileOutputStream fos=new FileOutputStream(fx);
 				byte b[]=new byte[n+m];
@@ -66,19 +60,8 @@ public class TestFile {
 				fis2.read(b);
 				fis2.close();
 				fos2.write(b);
-			//	fos2.flush();
-//				if(i!=3) {
-//					System.arraycopy(b, 0, src, i*n, n);
-//				}
-//				else {
-//					System.arraycopy(b, 0, src, i*n, n+m);
-//				}
 			}
 			fos2.close();
-//			File newf=new File(file.getParent()+"/副本"+file.getName());
-//			FileOutputStream fos2=new FileOutputStream(newf);
-//			fos2.write(src);
-//			fos2.close();
 		}
 		catch(IOException e) {
 			e.printStackTrace();
