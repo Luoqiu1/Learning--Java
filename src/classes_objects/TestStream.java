9618937fb31e9122e47a5d23ba59f9e6d30f32ea
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2020-04-01 22:43:33 +0800

    数据流的输入输出！
    类比最开始的输入流那样理解
    Scanner s=new Scanner(System.in);
    int i=s.newxInt().
    这种就是从用户输入一个数。
    改变一下方式，从txt中输入！代替了人工的操作。之后也不过是把
    int i=s.nextInt()改成
    int i=dis.readInt()！
    其中dis是一个DataInputStream的实例！
    用文件挨个输入就如同类比用户手动按格式挨个输入！

--- src/classes_objects/TestStream.java
+++ src/classes_objects/TestStream.java
@@ -5,29 +5,59 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.PrintWriter;
+import java.util.Scanner;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
 
 public class TestStream {
+	static int x=66;
+	static int y=77;
 	public static void main(String[] args) {
-		File f=new File("C:\\Users\\57195\\Desktop\\Learning--Java\\src\\classes_objects\\TestFile.java");
-		removeContents(f);
+		bufferWriteAndRead();
+		System.out.println();
+		dataWriteAndRead();
 	}
-	public static void removeContents(File f) {
-		StringBuffer sb=new StringBuffer();
-		try(FileReader fr=new FileReader(f);BufferedReader br=new BufferedReader(fr);){
-			while(true) {
-				String s=br.readLine();
-				if(s!=null) {
-					if(!s.trim().startsWith("//"))
-						sb.append(s+"\r\n");
-				}
-				else break;
-			}
+	public static void bufferWriteAndRead() {
+		File f=new File("c:/data/lol.txt");
+		try(FileWriter fw=new FileWriter(f);PrintWriter pw=new PrintWriter(fw)
+				;FileReader fr=new FileReader(f);BufferedReader br=new BufferedReader(fr)){
+			pw.print(x);pw.print('@');pw.print(y);
+			pw.flush();//这句话记得一定要有！
+			String s=br.readLine();
+			String str[]=s.split("@");
+			System.out.println(Integer.parseInt(str[0]));
+			System.out.println(Integer.parseInt(str[1]));
 		}
 		catch(IOException e) {
 			e.printStackTrace();
 		}
-		try(FileWriter fw=new FileWriter(f);PrintWriter pw=new PrintWriter(fw)){
-			pw.print(sb);
+//		try(FileReader fr=new FileReader(f);BufferedReader br=new BufferedReader(fr)){
+//		//	String s=br.readLine();
+//			String s=br.readLine();
+//			String str[]=s.split("@");
+//			System.out.println(Integer.parseInt(str[0]));
+//			System.out.println(Integer.parseInt(str[1]));
+//		}
+//		catch(IOException e) {
+//			e.printStackTrace();
+//		}
+	}
+	public static void dataWriteAndRead() {
+		File f=new File("c:/data/lol.txt");
+		try(FileOutputStream fos=new FileOutputStream(f);DataOutputStream dos=new DataOutputStream(fos)){
+			dos.writeInt(x);dos.writeUTF("Hello!");dos.writeInt(y);
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		try(FileInputStream fis=new FileInputStream(f);DataInputStream dis=new DataInputStream(fis)){
+			int a=dis.readInt();
+			String s=dis.readUTF();
+			int b=dis.readInt();
+			System.out.println(s);
+			System.out.println(a);System.out.println(b);
 		}
 		catch(IOException e) {
 			e.printStackTrace();
