090d1c4dd7c0a5b3e56044f12bba46e04eebe1a6
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2020-04-02 22:07:37 +0800

    字节流再探！理解再加深
    fis是一个FileInputStream对象，实际工作中通常是一段一段地读，而不是一开始学那种byte cs[]=new byte[(int)f.length()];
    这样做的话当文件很大时，一次性读不下来！超过了内存的大小。
    这时就要一段一段byte buffer[]=new byte[1024];来操作了（具体是1024或者其他的都可以 看实际要求）
    read()会返回实际的长度，再配合write(buffer,0,实际长度)来实际写入。因为最后一段可能没有1024这么长，全写入的话会写入多余的东西！
    正确例如下：
    int actuallyReaded=fis.read(buffer);//读入buffer中
    fos.write(buffer,0,actuallyReaded);//从buffer中写出到文件里

--- src/classes_objects/TestStream.java
+++ src/classes_objects/TestStream.java
@@ -13,81 +13,30 @@ import java.io.FileOutputStream;
 import java.io.FileInputStream;
 
 
-	public class TestStream {
-		 public static void main(String[] args) {
-			File f=new File("C:\\Users\\57195\\Desktop\\Learning--Java\\src\\classes_objects/Class.java");
-			StringBuffer s=new StringBuffer();
-//			try(FileReader fr=new FileReader(f);BufferedReader br=new BufferedReader(fr)){
-//				while(true) {
-//					String str=br.readLine();
-//					if(str==null)break;
-//					s.append(str+"\r\n");
-//				}
-//			}
-			String sx = null;
-			try(FileReader fr=new FileReader(f);){
-				char c[]=new char[(int)f.length()];
-				fr.read(c);
-				sx=new String(c);
+public class TestStream {
+	public static void main(String[] args) {
+		copyFile("C:\\Data/test.txt","C:\\Data/test2.txt");
+	}
+	public static void copyFile(String srcFile, String destFile) {
+		File f1=new File(srcFile);
+		File f2=new File(destFile);
+//		char cs[]=new char[(int)f1.length()];
+		byte buffer[]=new byte[1024];
+//		try(FileReader fr=new FileReader(f1);FileWriter fw=new FileWriter(f2)){
+//			fr.read(cs);
+//			fw.write(cs);
+//		}
+		try(FileInputStream fis=new FileInputStream(f1);FileOutputStream fos=new FileOutputStream(f2)) {
+			while(true) {
+				int actuallyReaded=fis.read(buffer);
+				if(actuallyReaded==-1)break;
+				fos.write(buffer,0,actuallyReaded);
+				//如果不取实际的直接全部写上，会有多出来的0！
 			}
-			catch(IOException e) {
-				e.printStackTrace();
-			}
-			Scanner in=new Scanner(System.in);
-			System.out.println("请输入类的名称：");
-			String Class;Class=in.next();
-			System.out.println("请输入属性的类型");
-			String Type;Type=in.next();
-			System.out.println("请输入属性的名称");
-			String Name;Name=in.next();
-			char Mid[]=Name.toCharArray();
-			Mid[0]=Character.toUpperCase(Mid[0]);
-			String UName=new String(Mid);
-			
-			sx=sx.replaceAll("@class@", Class);
-			sx=sx.replaceAll("@type@", Type);
-			sx=sx.replaceAll("@property@", Name);
-			sx=sx.replaceAll("@Uproperty@", UName);
-			
-//			File f2=new File("C:\\\\Users\\\\57195\\\\Desktop\\\\Learning--Java\\\\src\\\\classes_objects/"+Class+".java");
-			File f2=new File(f.getParentFile().getAbsolutePath(),Class+".java");
-//			try(FileWriter fw=new FileWriter(f2);PrintWriter pw=new PrintWriter(fw)){
-//				String mid=new String(s);
-//				String s0[]=mid.split("@");
-//				for (int i = 0; i < s0.length; i++) {
-//					if(s0[i].startsWith("class"))s0[i]=s0[i].replaceFirst("class", Class);
-//					else if(s0[i].startsWith("type"))s0[i]=s0[i].replaceFirst("type", Type);
-//					else if(s0[i].startsWith("property"))s0[i]=s0[i].replaceFirst("property",Name);
-//					else if(s0[i].startsWith("Uproperty"))s0[i]=s0[i].replaceFirst("Uproperty",UName);
-//				}
-//				for (int i = 0; i < s0.length; i++) {
-//					pw.print(s0[i]);
-//				}
-//			}
-			try(FileWriter fw=new FileWriter(f2);){
-				fw.write(sx);
-			}
-			catch(IOException e) {
-				e.printStackTrace();
-			}
-			System.out.println("替换后的内容：");
-//			try(FileReader fr=new FileReader(f2);BufferedReader br=new BufferedReader(fr)){
-//				while(true) {
-//					String s3;
-//					s3=br.readLine();
-//					if(s3==null)break;
-//					System.out.println(s3);
-//				}
-//			}
-			try(FileReader fr=new FileReader(f2);){
-				char c[]=new char[(int)f2.length()];
-				fr.read(c);
-				System.out.println(c);
-			}
-			catch(IOException e) {
-				e.printStackTrace();
-			}
-			System.out.println("文件保存在："+f2);
+		}
+		catch(IOException e) {
+			e.printStackTrace();
 		}
 	}
+}
 
