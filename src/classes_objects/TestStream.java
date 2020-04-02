8d9a2573737c469979b4ec9fe264e547ee6bd184
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2020-04-02 22:54:25 +0800

    流的应用再实验：复制文件夹（及其子文件夹）、文件内容查找
    文件、文件夹、流、递归算法等综合应用！

--- src/classes_objects/TestStream.java
+++ src/classes_objects/TestStream.java
@@ -15,7 +15,10 @@ import java.io.FileInputStream;
 
 public class TestStream {
 	public static void main(String[] args) {
-		copyFile("C:\\Data/test.txt","C:\\Data/test2.txt");
+	//	copyFile("C:\\Data/test.txt","C:\\Data/test2.txt");
+	//	copyFolder("C:/Data","c:/Data2");
+		File f=new File("C:\\Users\\57195\\Desktop\\Learning--Java");
+		search(f,"Hero");
 	}
 	public static void copyFile(String srcFile, String destFile) {
 		File f1=new File(srcFile);
@@ -36,6 +39,49 @@ public class TestStream {
 		}
 		catch(IOException e) {
 			e.printStackTrace();
+		}
+	}
+	public static void copyFolder(String srcFolder, String destFolder){
+		File f1=new File(srcFolder);
+		File f2=new File(destFolder);
+		if(!f1.isDirectory())return;
+		if(!f1.exists())return;
+		if(f2.isFile())return;
+		if(!f2.exists())f2.mkdirs();
+		File f[]=f1.listFiles();
+		for (int i = 0; i < f.length; i++) {
+			File copyf=new File(f2,f[i].getName());
+			if(f[i].isDirectory()) {
+				copyFolder(f[i].getAbsolutePath(),copyf.getAbsolutePath());
+			}
+			else {
+				copyFile(f[i].getAbsolutePath(),copyf.getAbsolutePath());
+			}
+		}
+	}
+	public static void search(File folder, String search) {
+		if(folder.isFile())return;
+		if(!folder.exists())return;
+		File f[]=folder.listFiles();
+		for (int i = 0; i < f.length; i++) {
+			String s=f[i].getAbsolutePath();
+			if(s.toLowerCase().endsWith(".java")) {
+				try(FileInputStream fis=new FileInputStream(f[i])){
+					byte cs[]=new byte[(int)f[i].length()];
+					fis.read(cs);
+					String s1=new String(cs);
+				//	if(-1!=s1.indexOf(search)) {
+					if(s1.contains(search)) {
+						System.out.printf("找到子目标字符串%s,在文件：%s%n",search,f[i].getAbsolutePath());
+					}
+				}
+				catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
+			else if(f[i].isDirectory()) {
+				search(f[i],search);
+			}
 		}
 	}
 }
