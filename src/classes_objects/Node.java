f2e4c1e2cdd86a891d3ec67177de46901c3c9ed5
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2020-04-04 22:02:48 +0800

    递归访问某一结点前一定注意先检查存在性！java二叉树创建及递归前中后序遍历
    	public void midGo(Node t) {//递归中序遍历
    		if(t.leftNode!=null)midGo(t.leftNode);
    		System.out.print(t.value+" ");
    		if(t.rightNode!=null)midGo(t.rightNode);
    		//在访问一个有参方法的时候，记得一定要先检查这个实参是否存在！
    		//因为在调用方法的时候会调用构造，构造一个新的实例！
    		//倘若这个实参本身就不存在，构造就会失败，抛出异常！
    	}
    
    上述代码中的if(t.leftNode!=null)即为判定条件！

---
+++ src/classes_objects/Node.java
@@ -0,0 +1,71 @@
+package classes_objects;
+import java.util.Scanner;
+
+
+public class Node {
+	public Node leftNode;
+	public Node rightNode;
+	public Object value;
+	public Node() {
+	}
+	public Node(Object v) {
+		value=v;
+	}
+	public void add(Object v) {
+		if(value==null) {
+			value=v;
+		}
+		else {
+			
+			if((int)v-(int)value<=0) {
+				if(leftNode==null) {
+					leftNode=new Node(v);
+				}
+				else {
+					leftNode.add(v);
+				}
+			}
+			else {
+				if(rightNode==null) {
+					rightNode=new Node(v);
+				}
+				else {
+					rightNode.add(v);
+				}
+			}
+		}
+	}
+	public void preGo(Node t) {//递归前序遍历
+		System.out.print(t.value+" ");
+		if(t.leftNode!=null)preGo(t.leftNode);
+		if(t.rightNode!=null)preGo(t.rightNode);
+		//在访问一个有参方法的时候，记得一定要先检查这个实参是否存在！
+		//因为在调用方法的时候会调用构造，构造一个新的实例！
+		//倘若这个实参本身就不存在，构造就会失败，抛出异常！
+	}
+	public void midGo(Node t) {//递归中序遍历
+		if(t.leftNode!=null)midGo(t.leftNode);
+		System.out.print(t.value+" ");
+		if(t.rightNode!=null)midGo(t.rightNode);
+		//在访问一个有参方法的时候，记得一定要先检查这个实参是否存在！
+		//因为在调用方法的时候会调用构造，构造一个新的实例！
+		//倘若这个实参本身就不存在，构造就会失败，抛出异常！
+	}
+	public void lastGo(Node t) {//递归后序遍历
+		if(t.leftNode!=null)lastGo(t.leftNode);
+		if(t.rightNode!=null)lastGo(t.rightNode);
+		System.out.print(t.value+" ");
+		//在访问一个有参方法的时候，记得一定要先检查这个实参是否存在！
+		//因为在调用方法的时候会调用构造，构造一个新的实例！
+		//倘若这个实参本身就不存在，构造就会失败，抛出异常！
+	}
+	public static void main(String[] args) {
+		Node Tree=new Node();
+		Scanner s=new Scanner(System.in);
+		for(int i=0;i<5;++i) {
+			int n=s.nextInt();
+			Tree.add(n);
+		}
+		Tree.midGo(Tree);System.out.println();
+	}
+}
