99de2e03bdc26d6348c1da73fb6a22c053734938
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2020-04-04 18:41:34 +0800

    用LinkedList实现MyStack
    stack数据结构是FILO，先进后出！一开始我的代码虽然结果是对的，但其实内部排列是错的！我是全部放在First，内部排列的逻辑正好反了！但表现出来的效果是一致的。仔细理解stack。可以通过“压入”、“弹出”来辅助加深理解。

---
+++ src/classes_objects/MyStack.java
@@ -0,0 +1,33 @@
+package classes_objects;
+import java.util.LinkedList;
+
+public class MyStack implements Stack{
+	private LinkedList<Hero> Heros=null;
+	public MyStack() {
+		Heros=new LinkedList<>();
+	}
+	public void push(Hero h) {
+//		Heros.addLast(h);
+		Heros.offer(h);
+	}
+	public Hero poll(){
+//		Hero x=Heros.getFirst();
+//		Heros.removeFirst();
+//		return x;
+		return Heros.removeLast();
+	}
+	public Hero peek(){
+		return Heros.getLast();
+	}
+	public static void main(String[] args) {
+		MyStack ms=new MyStack();
+		for (int i = 0; i < 5; i++) {
+			ms.push(new Hero("Hero "+i));
+		}
+		for (int i = 0; i < 5; i++) {
+			System.out.println(ms.peek());
+			Hero h=ms.poll();
+			System.out.println(h);
+		}
+	}
+}
---
+++ src/classes_objects/Stack.java
@@ -0,0 +1,7 @@
+package classes_objects;
+
+public interface Stack {
+	public void push(Hero h);
+	public Hero poll();
+	public Hero peek();
+}
