293a886d4854d2490e49761f5d194cd34ec20ce2
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2020-04-04 22:35:20 +0800

    面向过程与面向对象：通过两种对二叉树遍历的不同具体实现
    之前数据结构习惯了c在过程中间直接打印输出满足条件的值。
    而这次java中参考了网站中的代码，发现是在递归结束时返回一个List列表。
    从这两者之间窥探到了一丝面向对象与面向过程的不同之处。

--- src/classes_objects/Node.java
+++ src/classes_objects/Node.java
@@ -1,5 +1,7 @@
 package classes_objects;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 
 public class Node {
@@ -19,19 +21,25 @@ public class Node {
 			
 			if((int)v-(int)value<=0) {
 				if(leftNode==null) {
-					leftNode=new Node(v);
+					leftNode=new Node();
+					//这里也可以是直接构造new Node(v);
+					//但这样一来要再一次递归下去查找创建的情况
+					//就得放在else里了
+					// if(leftNode==null) leftNode=new Node(v);
+					// else leftNode.add(v)
+					//两种方法均可。
 				}
-				else {
+				
 					leftNode.add(v);
-				}
+				
 			}
 			else {
 				if(rightNode==null) {
-					rightNode=new Node(v);
+					rightNode=new Node();
 				}
-				else {
+				
 					rightNode.add(v);
-				}
+				
 			}
 		}
 	}
@@ -43,13 +51,23 @@ public class Node {
 		//因为在调用方法的时候会调用构造，构造一个新的实例！
 		//倘若这个实参本身就不存在，构造就会失败，抛出异常！
 	}
-	public void midGo(Node t) {//递归中序遍历
+	public void midGo(Node t) {//递归中序遍历 面向过程的思维习惯
+							   //在过程中直接输出
 		if(t.leftNode!=null)midGo(t.leftNode);
 		System.out.print(t.value+" ");
 		if(t.rightNode!=null)midGo(t.rightNode);
 		//在访问一个有参方法的时候，记得一定要先检查这个实参是否存在！
 		//因为在调用方法的时候会调用构造，构造一个新的实例！
 		//倘若这个实参本身就不存在，构造就会失败，抛出异常！
+	}
+	public List<Object> dataMidGo(){//返回列表的递归中序遍历
+									//面向对象的思维习惯，在递归结束时输出对象
+		List<Object> values=new ArrayList<>();
+		if(leftNode!=null)values.addAll(leftNode.dataMidGo());
+		values.add(value);
+		if(rightNode!=null)values.addAll(rightNode.dataMidGo());
+		System.out.println(values);
+		return values;
 	}
 	public void lastGo(Node t) {//递归后序遍历
 		if(t.leftNode!=null)lastGo(t.leftNode);
@@ -59,6 +77,7 @@ public class Node {
 		//因为在调用方法的时候会调用构造，构造一个新的实例！
 		//倘若这个实参本身就不存在，构造就会失败，抛出异常！
 	}
+	
 	public static void main(String[] args) {
 		Node Tree=new Node();
 		Scanner s=new Scanner(System.in);
@@ -67,5 +86,7 @@ public class Node {
 			Tree.add(n);
 		}
 		Tree.midGo(Tree);System.out.println();
+		List<Object> it=Tree.dataMidGo();
+		System.out.println(it);
 	}
 }
