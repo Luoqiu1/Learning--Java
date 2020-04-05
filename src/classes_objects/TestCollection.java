6f12c108f554827dda10b102d3d5cc206291be2d
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2020-04-05 23:16:41 +0800

    HashMap的理解及实现
    在对一个HashMap对象get的时候，关键字就算不存在也不会导致编译错误，可以理解成因为在表中不存在这个关键字，返回null！而不是说要去打开一个不存在的东西。这两者有区别。
    HashMap的创建再仔细去体会。

--- src/classes_objects/TestCollection.java
+++ src/classes_objects/TestCollection.java
@@ -1,34 +1,89 @@
 package classes_objects;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Iterator;
 
 public class TestCollection {
 	public static int n=20;
+	public static int m=3000000;
 	public static void main(String[] args) {
-		List<Integer> list=new ArrayList<>();
-		for(int i=0;i<n;++i) {
-			list.add((int)(Math.random()*40000));
+//		List<Integer> list=new ArrayList<>();
+//		for(int i=0;i<n;++i) {
+//			list.add((int)(Math.random()*40000));
+//		}
+//		List<Integer> listCopy=new ArrayList<>(list);
+////		System.out.println(list);
+////		System.out.println(listCopy);
+//		List<Integer> listCopy2=new ArrayList<>(list);
+//		System.out.println(listCopy);
+//		System.out.println(listCopy2);
+//		System.out.println(list);	
+//		listCopy2=binarySort(listCopy2);
+//	//	binarySort(listCopy2);
+//		bubbleSort(list);
+////		System.out.println(list);
+////		System.out.println(listCopy);
+//		selectionSort(listCopy);
+////		System.out.println(list);
+//		System.out.println("比较选择法跟冒泡法数组是否相同："+list.equals(listCopy));
+//		System.out.println("比较选择法跟二叉树数组是否相同："+listCopy.equals(listCopy2));
+//		System.out.println("选择法："+listCopy);
+//		System.out.println("二叉树法："+listCopy2);
+//		System.out.println("冒泡法："+list);	
+		
+		List<Hero> lst=new ArrayList<>();
+		for(int i=0;i<m;++i) {
+		//	lst.add(new Hero("Hero-"+(int)(Math.random())*9000+1000));
+			//草啊。。仔细一点。如果不稳的话就分设多个变量来
+			//括号太多容易看不清
+			//上面注释这里，其实就是恒等于1000了...
+			//(int)(Math.random())*9000这里恒等于0！。。
+			lst.add(new Hero("Hero-"+(int)(Math.random()*9000+1000)));
 		}
-		List<Integer> listCopy=new ArrayList<>(list);
-//		System.out.println(list);
-//		System.out.println(listCopy);
-		List<Integer> listCopy2=new ArrayList<>(list);
-		System.out.println(listCopy);
-		System.out.println(listCopy2);
-		System.out.println(list);	
-		listCopy2=binarySort(listCopy2);
-	//	binarySort(listCopy2);
-		bubbleSort(list);
-//		System.out.println(list);
-//		System.out.println(listCopy);
-		selectionSort(listCopy);
-//		System.out.println(list);
-		System.out.println("比较选择法跟冒泡法数组是否相同："+list.equals(listCopy));
-		System.out.println("比较选择法跟二叉树数组是否相同："+listCopy.equals(listCopy2));
-		System.out.println("选择法："+listCopy);
-		System.out.println("二叉树法："+listCopy2);
-		System.out.println("冒泡法："+list);	
+		HashMap<String,List<Hero>> hashmap=new HashMap<>();
+//		List<Hero> hee=hashmap.get("ss");
+//		if(hee==null)System.out.println("yesss");
+		for(Hero h:lst) {
+			List<Hero> hs=hashmap.get(h.name);
+			if(hs==null) {
+				hs=new ArrayList<>();
+				hashmap.put(h.name,hs);
+			}
+			hs.add(h);
+		}
+		List<Hero> h1=//hashmap.get("Hero-5555");
+		searchHashMap(hashmap,"Hero-5555");
+		List<Hero> h2=
+		searchNotHashMap(lst,"Hero-5555");
+		System.out.println("大小："+h1.size());
+		System.out.println("大小："+h2.size());
+		System.out.println((h2.equals(h1)));
+	//	System.out.println(h1);System.out.println(h2);
+		
+	}
+	public static List<Hero> searchHashMap(HashMap<String,List<Hero>> hm,String name) {
+		List<Hero> hs;
+		long ss1=System.currentTimeMillis();
+		hs=hm.get(name);
+		long ss2=System.currentTimeMillis();
+		System.out.printf("哈希表花费：%dss%n",ss2-ss1);
+		return hs;
+	}
+	public static List<Hero> searchNotHashMap(List<Hero> list,String name) {
+		List<Hero> hs=new ArrayList<>();
+		long ss1=System.currentTimeMillis();
+		for(Hero h:list) {
+		//	if(h.name=="Hero-5555")hs.add(h);
+			//怎么又是这个经典错误......==与equals！
+			if(h.name.equals(name))hs.add(h);
+		}
+		long ss2=System.currentTimeMillis();
+		System.out.printf("迭代花费：%dss%n",ss2-ss1);
+		return hs;
+	}
+	public static void searchNotHasMap(List<Integer> list) {
+		
 	}
 	public static void bubbleSort(List<Integer> list) {
 	//	List<int> list=new ArrayList<>();
