477fc468747f60c88952b96d764f65ff5f2b8c15
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2020-04-08 19:51:21 +0800

    泛型之间不能类型转换！这也是泛型通配符出现的意义之一！
    子类泛型无法转化成父类泛型；父类泛型同样不能转换成子类泛型。
    通过Hero，ADHero，APHero父类及两个子类之间进行理解。

--- src/classes_objects/TestGeneric.java
+++ src/classes_objects/TestGeneric.java
@@ -18,12 +18,13 @@ public class TestGeneric {
  
     public static void main(String[] args) {
         ArrayList<Hero> hs = new ArrayList<>();
-   //     ArrayList<APHero> aphs = new ArrayList<>();
-        ArrayList<ADHero> adhs = new ArrayList<>();
- 
-        iterate(hs);
-   //     iterateAP(aphs);
-        iterate(adhs);
+        ArrayList<ADHero> adh=new ArrayList<>();
+        hs.add(new ADHero());
+        hs=adh;
+        adh=hs;
+        //上面两行均不可转换
+        //即子类泛型不能转换成父类泛型
+        //父类泛型也不能转换成子类泛型
     }
  
 }
