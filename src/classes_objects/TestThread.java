0c50d30baa7f652e77c00003d76a096ca327aa7b
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2020-04-11 11:58:46 +0800

    synchronized因为设计方法上的缺陷可能导致的死锁问题
    当线程t1占用对象a的时候，又想去占用对象b。而此时线程t2占用了对象b。则线程t1必须等待对象b被释放出来之后才能继续运行下去（占用b）。如果此时线程t2在占用对象b的时候也想去占用对象a，则线程t2也会等待着对象a被释放。这样一来两个线程相互等待，谁都没有释放自己占用着的对象。无限等着下去，就形成了死锁。
    所以尽量要避免在synchronized一个对象后在代码里面又synchronized另一个对象的设计风格！

--- src/classes_objects/TestThread.java
+++ src/classes_objects/TestThread.java
@@ -7,104 +7,67 @@ import java.util.ArrayList;
 import java.util.List;
  
 public class TestThread {
- 
-    /**
-     * @param file 查找的目录
-     * @param search 查找的字符串
-     */
-    public static void search(File file, String search) {
-        if (file.isFile()) {
-            if(file.getName().toLowerCase().endsWith(".java")){
-//                String fileContent = readFileConent(file);
-//                if(fileContent.contains(search)){
-//                    System.out.printf("找到子目标字符串%s,在文件:%s%n",search,file);
-//                }
-//            	SearchFileThread sft=new SearchFileThread(file,search);
-//            	sft.start();
-//            	new SearchFileThread(file,search).start();
-            	SearchFileThread sft=new SearchFileThread(file,search);
-            	new Thread(sft).start();
-            }
-        }
-        if (file.isDirectory()) {
-            File[] fs = file.listFiles();
-            for (File f : fs) {
-                search(f, search);
-            }
-        }
-    }
-     
-    public static String readFileConent(File file){
-        try (FileReader fr = new FileReader(file)) {
-            char[] all = new char[(int) file.length()];
-            fr.read(all);
-            return new String(all);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
- 
-    }
-    public static boolean found = false;
+    
     public static void main(String[] args) {
-        String password = randomString(3);
-        System.out.println("密码是:" + password);
-        char[] guessPassword = new char[password.length()];
-        List<String> midAns=new ArrayList<>();
-        GeneratePasswordLog tLog=new GeneratePasswordLog(midAns);
-//        tLog.setDaemon(true);
-        tLog.start();
-        Thread t=new Thread(new GeneratePasswordThread(guessPassword,password,midAns));
-//        t.setDaemon(true);
-        t.start();
-//        generatePassword(guessPassword,password);
-        
-    }
-     
-    public static void generatePassword(char[] guessPassword,String password){
-        generatePassword(guessPassword,0,password);
-    }
-     
-    public static void generatePassword(char[] guessPassword,int index,String password){
-        if(found)
-            return;
-        for (short i = '0'; i <= 'z'; i++) {
-            char c = (char)i;
-            if(!Character.isLetterOrDigit(c))
-                continue;
-            guessPassword[index] = c;
-            if(index!=guessPassword.length-1){
-                generatePassword(guessPassword,index+1,password);
-            }
-            else{
-                String guess = new String(guessPassword);
-//              System.out.println("穷举出来的密码是：" + guess);
-                if(guess.equals(password)){
-                    System.out.println("找到了，密码是" + guess);
-                    found =true;
-                    return;
-                }
-            }
-        }
-    }
- 
-    private static String randomString(int length) {
-        String pool = "";
-        for (short i = '0'; i <= '9'; i++) {
-            pool += (char) i;
-        }
-        for (short i = 'a'; i <= 'z'; i++) {
-            pool += (char) i;
-        }
-        for (short i = 'A'; i <= 'Z'; i++) {
-            pool += (char) i;
-        }
-        char cs[] = new char[length];
-        for (int i = 0; i < cs.length; i++) {
-            int index = (int) (Math.random() * pool.length());
-            cs[i] = pool.charAt(index);
-        }
-        String result = new String(cs);
-        return result;
+    	Hero h1=new Hero("盖伦");
+    	Hero h2=new Hero("VN");
+    	Hero h3=new Hero("冰鸟");
+    	Thread t1=new Thread() {
+    		public void run() {
+    			synchronized(h1) {
+    				System.out.println("t1已占领对象 "+h1.name);
+//    				try {
+//        				Thread.sleep(1000);
+//        			}
+//    				catch(Exception e) {
+//    					e.printStackTrace();
+//    				}
+    				System.out.println("t1试图占领对象 "+h2.name);
+    				System.out.println("t1等待中。。。");
+    				synchronized(h2) {
+    					System.out.println("t1已成功占领对象 "+h2.name);
+    				}
+    			}
+    		}
+    	};
+    	t1.start();
+    	Thread t2=new Thread() {
+    		public void run() {
+    			synchronized(h2) {
+    				System.out.println("t2已占领对象 "+h3.name);
+//    				try {
+//        				Thread.sleep(1000);
+//        			}
+//    				catch(Exception e) {
+//    					e.printStackTrace();
+//    				}
+    				System.out.println("t2试图占领对象 "+h3.name);
+    				System.out.println("t2等待中。。。");
+    				synchronized(h3) {
+    					System.out.println("t2已成功占领对象 "+h3.name);
+    				}
+    			}
+    		}
+    	};
+    	t2.start();
+    	Thread t3=new Thread() {
+    		public void run() {
+    			synchronized(h3) {
+    				System.out.println("t3已占领对象 "+h3.name);
+//    				try {
+//        				Thread.sleep(1000);
+//        			}
+//    				catch(Exception e) {
+//    					e.printStackTrace();
+//    				}
+    				System.out.println("t3试图占领对象 "+h1.name);
+    				System.out.println("t3等待中。。。");
+    				synchronized(h1) {
+    					System.out.println("t3已成功占领对象 "+h1.name);
+    				}
+    			}
+    		}
+    	};
+    	t3.start();
     }
 }
