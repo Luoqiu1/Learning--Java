2a1405bc34a85dbfc5ce0369387c1de1ef72b61a
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2020-04-12 21:00:39 +0800

    使用自定义的ThreadPool与java自带的ThreadPoolExecutor线程池来实现查找文件，对比不同！
    ThreadPoolExecutor在包java.util.concurrent.下
    分别是Executor,TimeUnit,LinkedBlockingQueue,
    使用这个自带的线程池时，
    参数格式类比为：ThreadPoolExecutor threadPool= new ThreadPoolExecutor(10, 15, 60, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
    第一个参数10 表示这个线程池初始化了10个线程在里面工作
    第二个参数15 表示如果10个线程不够用了，就会自动增加到最多15个线程
    第三个参数60 结合第四个参数TimeUnit.SECONDS，表示经过60秒，多出来的线程还没有接到活儿，就会回收，最后保持池子里就10个
    第四个参数TimeUnit.SECONDS 如上
    第五个参数 new LinkedBlockingQueue() 用来放任务的集合
    
    execute方法用于添加新的任务

--- src/classes_objects/ThreadPool.java
+++ src/classes_objects/ThreadPool.java
@@ -1,14 +1,22 @@
 package classes_objects;
 
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileReader;
+import java.sql.Time;
+//import java.util.ThreadPoolExecutor;
+//import java.util.Concurrent;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class ThreadPool {
 	ArrayList<Runnable> tasks = new ArrayList<>();
-	int threadSize;
+	int threadPoolSize;
 
 	public ThreadPool(int size) {
-		this.threadSize = size;
-		for (int i = 0; i < threadSize; i++) {
+		this.threadPoolSize = size;
+		for (int i = 0; i < threadPoolSize; i++) {
 //			synchronized(tasks) {
 			//这里的synchronized没必要！
 			//追踪下面的new之后的start方法去探究
@@ -55,6 +63,9 @@ public class ThreadPool {
 					}
 					task=tasks.remove(tasks.size()-1);
 	//				tasks.notify();
+					//这里没必要再唤醒其他线程
+					//添加一个任务就在添加的地方唤醒即可！
+					//这里多余了
 				}
 				System.out.println("任务已被 "+this.getName()+" 接到进行处理！");
 				task.run();
@@ -62,31 +73,73 @@ public class ThreadPool {
 		}
 	}
 	public static void main(String[] args) {
-		ThreadPool threadPool=new ThreadPool(5);
-		try {
-			Thread.sleep(10);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		int gap=1000;
-		while(true) {
-			threadPool.add(new Runnable() {
-				public void run() {
-					try {
-						Thread.sleep(10000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-			});
-			try {
-				Thread.sleep(gap);
-				gap=gap>100?gap-100:100;
-			}
-			catch(Exception e) {
-				e.printStackTrace();
-			}
-		}
+//		ThreadPool threadPool=new ThreadPool(5);
+//		try {
+//			Thread.sleep(10);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//		int gap=1000;
+//		while(true) {
+//			threadPool.add(new Runnable() {
+//				public void run() {
+//					try {
+//						Thread.sleep(100);
+//					} catch (InterruptedException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+//				}
+//			});
+//			try {
+//				Thread.sleep(gap);
+//				gap=gap>100?gap-100:100;
+//			}
+//			catch(Exception e) {
+//				e.printStackTrace();
+//			}
+//		}
+		
+//		ThreadPoolExecutor threadPool=new ThreadPoolExecutor(10,15,60,TimeUnit.SECONDS,new LinkedBlockingQueue<Runnable>());
+				ThreadPool threadPool=new ThreadPool(10);
+		long s1=System.currentTimeMillis();
+		File folder =new File("C:\\Users\\57195\\Desktop\\Learning--Java");
+        search(folder,"Hero",threadPool);
+        long s2=System.currentTimeMillis();
+        System.out.println("查找完毕经过了："+(s2-s1)+"ss");
 	}
+//	public static void search(File file, String search,ThreadPoolExecutor threadPool) {
+	public static void search(File file, String search,ThreadPool threadPool) {
+		if (file.isFile()) {
+            if(file.getName().toLowerCase().endsWith(".java")){
+                //当找到.java文件的时候，就启动一个线程，进行专门的查找
+                Runnable r=new Runnable() {
+                	public void run() {
+                		String fileContent = readFileConent(file);
+                        if(fileContent.contains(search)){
+                            System.out.printf("找到子目标字符串%s,在文件:%s%n",search,file);
+                        }
+                	}
+                };
+            threadPool.add(r);
+//            threadPool.execute(r);
+            }
+        }
+        if (file.isDirectory()) {
+            File[] fs = file.listFiles();
+            for (File f : fs) {
+                search(f, search, threadPool);
+            }
+        }
+    }
+    static public String readFileConent(File file){
+            try (FileReader fr = new FileReader(file)) {
+                char[] all = new char[(int) file.length()];
+                fr.read(all);
+                return new String(all);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        } 
 }
