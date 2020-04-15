755def0c2261e0f63d8a4059ec119af18055e653
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2020-04-15 23:38:21 +0800

    JDBC的查询操作，以及一些细节问题
    executeQuery 执行SQL查询语句
    注意： 在取第二列的数据的时候，用的是rs.get(2) ，而不是get(1). 这个是整个Java自带的api里唯二的地方，使用基1的，即2就代表第二个。
    另一个地方是在PreparedStatement这里
    
    查询集、结果集用ResultSet对象来接受，操作还是用Statement对象的execute相关方法！

--- src/classes_objects/TestJDBC.java
+++ src/classes_objects/TestJDBC.java
@@ -3,50 +3,106 @@ package classes_objects;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 
 public class TestJDBC{
 	public static void main(String args[]) {
+//		try {
+//			Class.forName("com.mysql.jdbc.Driver");
+//		} catch (ClassNotFoundException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		}
+//		try(Connection c=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/"+
+//				"how2java?characterEncoding=UTF-8","root","admin");
+//				Statement s=c.createStatement()){
+//			
+//			
+////			Connection c=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/"+
+////			"how2java?characterEncoding=UTF-8","root","admin");
+////			Statement s=c.createStatement();
+//	//		System.out.println("获取Statement对象+"+s);
+////			String sql = 
+////					"insert into hero values(null,"+"'提莫'"+","+313.0f+","+50+")";
+//			//		"insert into hero values(null,'提莫',313.0f,50)";--这条编译错误
+//			//		"insert into hero values(null,"+'提莫'+","+313.0f+","+50+")";错误
+////					"insert into hero values(null,'提莫',313.0,50)";
+//					//发现小错误......
+////			System.out.println(sql);
+////			s.execute(sql);
+////			s.close();
+////			c.close();
+////			
+////			for(int i=0;i<100;++i) {
+////				int temp=1+i;
+////				int tempNumber=i+1;
+////				String sql1="insert into hero values("+temp+","
+////			+"'英雄"+tempNumber+"',"+Math.random()*100+","+Math.random()*50+100+")";
+////				s.execute(sql1);
+////				System.out.println("成功插入sql:"+sql1);
+////			}
+////			
+////			for(int i=100;i<300;++i) {
+////				String sql1="delete from hero where id="+i;
+////				System.out.println("成功删除:"+sql1);
+////				s.execute(sql1);
+////			}
+//			
+//			String sql="select * from hero";
+//			ResultSet rs=s.executeQuery(sql);
+//			while(rs.next()) {
+//				int id=rs.getInt("id");
+//				String name=rs.getString("name");
+//				double hp=rs.getFloat(3);
+//				int damage=rs.getInt("damage");
+//				System.out.printf("查询到了，该数据为：%nid:%d\tname:%s\thp:%f\tdamage:%d%n",id,name,hp,damage);
+//			}
+//			String name="dashen";String password="thisispassword";
+//			String sql2="select * from user where name='"+name+"'and password='"+password+"';";
+//			ResultSet rs2=s.executeQuery(sql2);
+//			if(rs2.next()) {
+//				System.out.println("账号密码正确！");
+//			}
+//			else {
+//				System.out.println("账号密码错误！");
+//			}
+//			String sql3="select count(*) from hero";
+//			ResultSet rs3=s.executeQuery(sql3);
+//			if(rs3.next()) {
+//				System.out.println("总共有 "+rs3.getInt(1)+" 个数据");
+//			}
+//			
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			if(e instanceof SQLException)
+//			e.printStackTrace();
+//		}
+		list(6,10);
+	}
+	public static void list(int start,int count) {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-		} catch (ClassNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
 		}
-		try(Connection c=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/"+
-				"how2java?characterEncoding=UTF-8","root","admin");
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		try(Connection c=DriverManager.getConnection("jdbc:mysql:"
+				+ "//127.0.0.1:3306/how2java?characterEncoding=UTF-8","root","admin");
 				Statement s=c.createStatement()){
-			
-			
-//			Connection c=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/"+
-//			"how2java?characterEncoding=UTF-8","root","admin");
-//			Statement s=c.createStatement();
-	//		System.out.println("获取Statement对象+"+s);
-//			String sql = 
-//					"insert into hero values(null,"+"'提莫'"+","+313.0f+","+50+")";
-			//		"insert into hero values(null,'提莫',313.0f,50)";--这条编译错误
-			//		"insert into hero values(null,"+'提莫'+","+313.0f+","+50+")";错误
-//					"insert into hero values(null,'提莫',313.0,50)";
-					//发现小错误......
-//			System.out.println(sql);
-//			s.execute(sql);
-//			s.close();
-//			c.close();
-			
-			for(int i=0;i<100;++i) {
-				int temp=100+i;
-				int tempNumber=i+1;
-				String sql1="insert into hero values("+temp+","
-			+"'英雄"+tempNumber+"',"+Math.random()*100+","+Math.random()*50+100+")";
-				s.execute(sql1);
-				System.out.println("成功插入sql:"+sql1);
+			String database="hero";
+			String sql="select * from "+database+" limit "+start+","+count;
+			ResultSet rs=s.executeQuery(sql);
+			while(rs.next()) {
+				int id=rs.getInt("id");
+				String name=rs.getString("name");
+				double hp=rs.getFloat(3);
+				int damage=rs.getInt("damage");
+				System.out.printf("查询到了，该数据为：%nid:%d\tname:%s\thp:%f\tdamage:%d%n",id,name,hp,damage);
 			}
-			
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			if(e instanceof SQLException)
+		}
+		catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
