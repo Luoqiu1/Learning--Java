90193c6d97a828644364a41ab736ed5af231e736
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2020-04-19 23:07:45 +0800

    直接用预编译Statement，与上一条提交中的有一些需要注意的点
    直接对一个PrepareStatement对象execute(sql)的话，这个预编译对象本身也会改！就像一个Statement对象一样，执行之后本身的String会变成这个sql...!
    然后是程序发生了不知错误，一开始运行得好好的都出来了，后来出了错，用了教程上的代码也错了，找不到问题暂时

--- src/classes_objects/TestJDBC.java
+++ src/classes_objects/TestJDBC.java
@@ -1,5 +1,5 @@
 package classes_objects;
-
+import java.util.Scanner;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.DriverManager;
@@ -170,47 +170,147 @@ public class TestJDBC{
 //		T1.start();
 //		T2.start();
 		
+//		try {
+//			Class.forName("com.mysql.jdbc.Driver");
+//		} catch (ClassNotFoundException e1) {
+//			e1.printStackTrace();
+//		}
+//		String sql="insert into hero values(null,?,40,40)";
+//		try(Connection c=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/"+
+//				"how2java?characterEncoding=UTF-8","root","admin");
+//				PreparedStatement ps=c.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS)){
+//			ps.setString(1, "哦牛");
+//			ps.execute();
+//			ResultSet rs=ps.getGeneratedKeys();
+//			//注意这里！！必须要有 rs.next()
+//			//就如同迭代器iterator中，使用if(it.next())一样
+//			//首先判断是否存在这个东西，防止非法访问
+//			//其次就是rs自增一步后，正式走到第一个可以访问的元素！
+//			//一开始这类像指针一样的东西都是在可能出现第一个可以访问的元素
+//			//的前一个位置！
+//			if(rs.next()) {
+//				int i=rs.getInt(1);
+//		//		System.out.println(rs);
+//				while(true) {
+//					--i;
+//					if(i==0)break;
+//					String sqltemp="select * from hero where id="+i;
+//					boolean flag=ps.execute(sqltemp);
+//					//可以将PrepareStatement当成Statement来用
+//					//参数位置放入一个String sql来执行！
+//					//就不用重新再创建一个Statement了
+//					if(flag) {
+//						sqltemp="delete from hero where id="+i;
+//						ps.execute(sqltemp);
+//						break;
+//					}
+//				}
+//			}
+//		}
+//		catch(Exception e) {
+//			e.printStackTrace();
+//		}
+		
+//		try {
+//			Class.forName("com.mysql.jdbc.Driver");
+//		} catch (ClassNotFoundException e1) {
+//			e1.printStackTrace();
+//		}
+//		String sql="select * from hero where id=";
+//		try(Connection c=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/"+
+//				"how2java?characterEncoding=UTF-8","root","admin");
+//				PreparedStatement ps=c.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS)){
+////			c.setAutoCommit(false);
+//			//----------------
+////			int n=0;
+////			int i=1;
+////			while(n<10) {
+////				while(true) {
+////					String stemp=sql+i;
+////			//		ResultSet rs=ps.executeQuery(stemp);
+////					
+////					if(rs.next()) {
+////			//		ps.setInt(1,i);
+////			//		boolean flag=ps.execute(stemp);
+////				//	if(ps.execute(stemp)) {
+////			//		if(flag) {
+////						System.out.println(stemp);
+////						String sqltemp="delete from hero where id="+i;
+////						ps.execute(sqltemp);
+////						n++;i++;break;
+////					}
+////					i++;
+////				}
+////			}
+//			c.setAutoCommit(false);
+//			String sqltemp="select id from hero order by id asc limit 0,10";
+//			Statement s=c.createStatement();
+//			ResultSet rs=s.executeQuery(sqltemp);
+//			//ResultSet rs=ps.executeQuery(sqltemp);
+//			
+//			while(rs.next()) {
+//				int i=rs.getInt(1);
+//				String stemp="delete from hero where id="+i;
+//				System.out.println("准备删除i="+i+"的数据");
+//				ps.execute(stemp);
+//			}
+//			
+//			System.out.println("是否删除的数据？Y/N");
+//			Scanner sx=new Scanner(System.in);
+//			String a=sx.next();
+//			System.out.println(a);
+//			if(a.charAt(0)=='Y') {
+//				System.out.println("提交删除！");
+//				c.commit();
+//			}
+//			else System.out.println("删除失败！");
+//		}
+//		catch(Exception e) {
+//			e.printStackTrace();
+//		}
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		} catch (ClassNotFoundException e1) {
-			e1.printStackTrace();
-		}
-		String sql="insert into hero values(null,?,40,40)";
-		try(Connection c=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/"+
-				"how2java?characterEncoding=UTF-8","root","admin");
-				PreparedStatement ps=c.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS)){
-			ps.setString(1, "哦牛");
-			ps.execute();
-			ResultSet rs=ps.getGeneratedKeys();
-			//注意这里！！必须要有 rs.next()
-			//就如同迭代器iterator中，使用if(it.next())一样
-			//首先判断是否存在这个东西，防止非法访问
-			//其次就是rs自增一步后，正式走到第一个可以访问的元素！
-			//一开始这类像指针一样的东西都是在可能出现第一个可以访问的元素
-			//的前一个位置！
-			if(rs.next()) {
-				int i=rs.getInt(1);
-		//		System.out.println(rs);
-				while(true) {
-					--i;
-					if(i==0)break;
-					String sqltemp="select * from hero where id="+i;
-					boolean flag=ps.execute(sqltemp);
-					//可以将PrepareStatement当成Statement来用
-					//参数位置放入一个String sql来执行！
-					//就不用重新再创建一个Statement了
-					if(flag) {
-						sqltemp="delete from hero where id="+i;
-						ps.execute(sqltemp);
-						break;
-					}
-				}
-			}
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+         
+        try (Connection c = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/how2java?characterEncoding=UTF-8",
+                "root", "admin");
+                Statement st4Query = c.createStatement();
+                Statement st4Delete = c.createStatement();
+                Scanner s = new Scanner(System.in);) {
+ 
+            //把自动提交关闭
+            c.setAutoCommit(false);
+            //查出前10条
+            ResultSet rs =st4Query.executeQuery("select id from Hero order by id asc limit 0,10 ");
+            while(rs.next()){
+                int id = rs.getInt(1);
+                System.out.println("试图删除id="+id+" 的数据");
+                st4Delete.execute("delete from Hero where id = " +id);
+            }
+             
+            //是否删除这10条
+            while(true){
+                System.out.println("是否要删除数据(Y/N)");
+                 
+                String str = s.next();
+                if ("Y".equals(str)) {
+                    //如果输入的是Y，则提交删除操作
+                    c.commit();
+                    System.out.println("提交删除");
+                    break;
+                } else if ("N".equals(str)) {
+                    System.out.println("放弃删除");
+                    break;
+                }
+            }
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+ 
+    }
 	public static void list(int start,int count) {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
