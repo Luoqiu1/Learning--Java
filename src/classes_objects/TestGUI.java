57e863242ded2852c9a2541a09f8571a1b275a75
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2020-04-21 23:15:01 +0800

    模态与大小变化练习；对默认关闭进程的理解加深；事件监听的嵌套
    除了JFrame外，还有一种窗口是JDialog。
    设置模态通过setModal方法来实现。
    注意，在模态窗口下不能使用自动关闭程序的代码，就是说不能使用setDefaultCloseOperation(...);因为设置了这个的意思是，当关闭了这个窗口，程序会跟着一起结束。而这只是模态子窗口，从逻辑上来说只是关闭这一个窗口，不需要结束进程！

--- src/classes_objects/TestGUI.java
+++ src/classes_objects/TestGUI.java
@@ -2,11 +2,14 @@ package classes_objects;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -73,59 +76,145 @@ public class TestGUI {
 //		t.setDaemon(true);
 //		t.start();
 		
+//		
+//		JFrame f=new JFrame("LOL");
+//		f.setSize(400,300);
+//		f.setLocation(580,200);
+//		f.setLayout(null);
+//	//	JLabel l=new JLabel("C:\\Data/shana.png");
+//		//虽然上一行注释处也可以这样做
+//		//但是这样就不方便调用图片的宽、高等参数了
+//		//整一个ImageIcon对象，调用其getIconWidth方法！
+//		JLabel l=new JLabel();
+//		ImageIcon i=new ImageIcon("C:\\Data/shana.png");
+//		l.setIcon(i);
+//		l.setBounds(50,50,i.getIconWidth(),i.getIconHeight());
+//		
+//		
+//		JButton b=new JButton("隐藏图片");
+//	//	b.setName("隐藏图片");
+//		b.setBounds(150,200,100,30);
+//		f.add(b);
+//		f.add(l);
+//		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		f.setVisible(true);
+//	//	System.out.println(b.getName());
+//	//	System.out.println(b.toString());
+//		ActionListener l1=new ActionListener() {
+//				boolean flag=true;
+//				public void actionPerformed(ActionEvent e){
+////					if(b.getName().equals("隐藏图片")) {
+////						b.setText("显示图片");
+////						b.setName("显示图片");
+////						l.setVisible(false);
+////						//理解一下。是要对内容（这个内容被设置在了标签中）显示
+////						//所以是应该对包含了这个内容的对象进行操作！
+////						//按钮如果隐藏了的话，那怎么进行事件操作呢？
+////					}
+////					else {
+////						b.setText("隐藏图片");
+////						b.setName("隐藏图片");
+////						l.setVisible(true);
+////					}
+//					//上注释是我自己通过setName,getName方法来完成的！
+//					//还有另一种方法是基于
+//					//每点一次按钮，必定发生变化。这种思想来处理简化：
+//					
+//					
+//					flag=!flag;
+//					l.setVisible(flag);
+//					String temp=flag?"隐藏图片":"显示图片";
+//					b.setText(temp);
+//					//这是个类，匿名类
+//					//所以有类属性，直接通过类属性来操作很方便！
+//				}
+//			};
+//		b.addActionListener(l1);
+		
+//		JFrame f=new JFrame("LOL");
+//		f.setSize(400,300);
+//		f.setLocation(580,200);
+//		f.setLayout(null);
+//		JLabel l=new JLabel();
+//		ImageIcon i=new ImageIcon("C:\\Data/shana.png");
+//		l.setIcon(i);
+//	//	l.setBounds(50,50,i.getIconWidth(),i.getIconHeight());
+//		l.setBounds(50,50,i.getIconWidth(),i.getIconHeight());
+//		System.out.println("图片的宽为："+i.getIconWidth()+" 高为："+i.getIconHeight());
+//		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		f.add(l);
+//		f.setVisible(true);
+//		f.addKeyListener(new KeyListener() {
+//			public void keyPressed(KeyEvent e) {
+//				switch(e.getKeyCode()) {	
+//				case 38:
+//					l.setLocation(l.getX(),l.getY()-10);break;
+//				case 40:
+//					l.setLocation(l.getX(),l.getY()+10);break;
+//				case 37:
+//					l.setLocation(l.getX()-10,l.getY());break;
+//				case 39:
+//					l.setLocation(l.getX()+10,l.getY());break;
+//				}
+//				System.out.println(e.getKeyCode()+" "+l.getX()+" "+l.getY());
+//			}
+//
+//			@Override
+//			public void keyTyped(KeyEvent e) {
+//				// TODO Auto-generated method stub
+//				
+//			}
+//
+//			@Override
+//			public void keyReleased(KeyEvent e) {
+//				// TODO Auto-generated method stub
+//				
+//			}
+//		});
 		
 		JFrame f=new JFrame("LOL");
 		f.setSize(400,300);
-		f.setLocation(580,200);
+		f.setLocation(200,200);
 		f.setLayout(null);
-	//	JLabel l=new JLabel("C:\\Data/shana.png");
-		//虽然上一行注释处也可以这样做
-		//但是这样就不方便调用图片的宽、高等参数了
-		//整一个ImageIcon对象，调用其getIconWidth方法！
-		JLabel l=new JLabel();
-		ImageIcon i=new ImageIcon("C:\\Data/shana.png");
-		l.setIcon(i);
-		l.setBounds(50,50,i.getIconWidth(),i.getIconHeight());
-		
-		
-		JButton b=new JButton("隐藏图片");
-	//	b.setName("隐藏图片");
-		b.setBounds(150,200,100,30);
+		JButton b=new JButton("打开一个模态窗口");
+		b.setBounds(50,50,280,30);
 		f.add(b);
-		f.add(l);
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ActionListener listener1=new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JDialog d=new JDialog(f,"模态窗口");
+			//	d.setTitle("模态窗口");
+				
+			//	d.setLocation(f.getX()+120,f.getY()+130);
+				d.setLocationRelativeTo(f);
+				//设置窗口相对于指定组件的位置。
+				
+				d.setSize(200,150);
+			//	d.setLayout(null);
+				d.setModal(true);
+				JButton b=new JButton("锁定大小");
+			//	b.setBounds(50,50,280,30);
+				//当窗口没有被设置为绝对布局时
+				//按钮会被填充完整个窗口
+				//所以这一行有没有都不影响最终效果
+				//从逻辑上来说也没必要写
+				
+				d.add(b);
+				ActionListener listener=new ActionListener() {
+					boolean flag=false;
+					public void actionPerformed(ActionEvent e) {
+						flag=!flag;
+						d.setResizable(flag);
+					}
+				};
+				b.addActionListener(listener);
+				d.setVisible(true);
+		//		d.setDefaultCloseOperation(JDialog.EXIT_ON_CLOSE);
+				//只有一个窗口需要设置这个
+				//被设置的窗口若被关闭，则程序结束！
+			}
+		};
+		b.addActionListener(listener1);
 		f.setVisible(true);
-	//	System.out.println(b.getName());
-	//	System.out.println(b.toString());
-		ActionListener l1=new ActionListener() {
-				boolean flag=true;
-				public void actionPerformed(ActionEvent e){
-//					if(b.getName().equals("隐藏图片")) {
-//						b.setText("显示图片");
-//						b.setName("显示图片");
-//						l.setVisible(false);
-//						//理解一下。是要对内容（这个内容被设置在了标签中）显示
-//						//所以是应该对包含了这个内容的对象进行操作！
-//						//按钮如果隐藏了的话，那怎么进行事件操作呢？
-//					}
-//					else {
-//						b.setText("隐藏图片");
-//						b.setName("隐藏图片");
-//						l.setVisible(true);
-//					}
-					//上注释是我自己通过setName,getName方法来完成的！
-					//还有另一种方法是基于
-					//每点一次按钮，必定发生变化。这种思想来处理简化：
-					
-					
-					flag=!flag;
-					l.setVisible(flag);
-					String temp=flag?"隐藏图片":"显示图片";
-					b.setText(temp);
-					//这是个类，匿名类
-					//所以有类属性，直接通过类属性来操作很方便！
-				}
-			};
-		b.addActionListener(l1);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
