6e649565bd026fa745bc894dcc5cc88528c6ad74
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2020-04-21 20:56:44 +0800

    GUI：事件监听
    先尝试去理解JLabel组件（后面会系统学）。在通过对按钮监听的实践之下慢慢体会JFrame、JButton、JLabel等类的操作及内涵。
    注释处写了一些理解。

--- src/classes_objects/TestGUI.java
+++ src/classes_objects/TestGUI.java
@@ -1,7 +1,12 @@
 package classes_objects;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -12,59 +17,115 @@ import java.io.Writer;
 
 public class TestGUI {
 	public static void main(String[] args) {
-		JFrame jf=new JFrame("LOL");
-		File f=new File("C:\\Data/location.txt");
-		String x="200",y="200";
-		if(f.exists()) {
-			if(f.length()!=0) {
-				try(FileInputStream fis=new FileInputStream(f)) {
-					byte b[]=new byte[(int)f.length()];
-					fis.read(b);
-					String s=new String(b);
-					String s2[]=s.split(" ");
-					x=s2[0];y=s2[1];
+//		JFrame jf=new JFrame("LOL");
+//		File f=new File("C:\\Data/location.txt");
+//		String x="200",y="200";
+//		if(f.exists()) {
+//			if(f.length()!=0) {
+//				try(FileInputStream fis=new FileInputStream(f)) {
+//					byte b[]=new byte[(int)f.length()];
+//					fis.read(b);
+//					String s=new String(b);
+//					String s2[]=s.split(" ");
+//					x=s2[0];y=s2[1];
+//				}
+//				catch (IOException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//			}
+//		}
+//		jf.setSize(400,300);
+//		jf.setLocation(Integer.parseInt(x),Integer.parseInt(y));
+//		jf.setLayout(null);
+//		JButton jb=new JButton("一键秒杀地方基地挂");
+//		jb.setBounds(50,50,280,30);
+//		jf.add(jb);
+//		jf.setVisible(true);
+//		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		Object o=new Object();
+//		Thread t=new Thread() {
+//			public void run() {
+//			//	synchronized(o) {
+//					while(true) {
+//						try {
+//						//	o.wait(100);
+//							Thread.sleep(100);
+//						} catch (InterruptedException e) {
+//							// TODO Auto-generated catch block
+//							e.printStackTrace();
+//						}
+//						File f=new File("C:\\Data/location.txt");
+//			//			FileOutputStream fos=new FileOutputStream(f);
+//						try(FileWriter w= new FileWriter(f);) {
+//							String x=String.valueOf(jf.getX());
+//							String y=String.valueOf(jf.getY());
+//							String temp=x+" "+y;
+//							w.write(temp);
+//						} catch (IOException e) {
+//							// TODO Auto-generated catch block
+//							e.printStackTrace();
+//						}
+//					}
+//			//	}
+//			}
+//		};
+//		t.setDaemon(true);
+//		t.start();
+		
+		
+		JFrame f=new JFrame("LOL");
+		f.setSize(400,300);
+		f.setLocation(580,200);
+		f.setLayout(null);
+	//	JLabel l=new JLabel("C:\\Data/shana.png");
+		//虽然上一行注释处也可以这样做
+		//但是这样就不方便调用图片的宽、高等参数了
+		//整一个ImageIcon对象，调用其getIconWidth方法！
+		JLabel l=new JLabel();
+		ImageIcon i=new ImageIcon("C:\\Data/shana.png");
+		l.setIcon(i);
+		l.setBounds(50,50,i.getIconWidth(),i.getIconHeight());
+		
+		
+		JButton b=new JButton("隐藏图片");
+	//	b.setName("隐藏图片");
+		b.setBounds(150,200,100,30);
+		f.add(b);
+		f.add(l);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+	//	System.out.println(b.getName());
+	//	System.out.println(b.toString());
+		ActionListener l1=new ActionListener() {
+				boolean flag=true;
+				public void actionPerformed(ActionEvent e){
+//					if(b.getName().equals("隐藏图片")) {
+//						b.setText("显示图片");
+//						b.setName("显示图片");
+//						l.setVisible(false);
+//						//理解一下。是要对内容（这个内容被设置在了标签中）显示
+//						//所以是应该对包含了这个内容的对象进行操作！
+//						//按钮如果隐藏了的话，那怎么进行事件操作呢？
+//					}
+//					else {
+//						b.setText("隐藏图片");
+//						b.setName("隐藏图片");
+//						l.setVisible(true);
+//					}
+					//上注释是我自己通过setName,getName方法来完成的！
+					//还有另一种方法是基于
+					//每点一次按钮，必定发生变化。这种思想来处理简化：
+					
+					
+					flag=!flag;
+					l.setVisible(flag);
+					String temp=flag?"隐藏图片":"显示图片";
+					b.setText(temp);
+					//这是个类，匿名类
+					//所以有类属性，直接通过类属性来操作很方便！
 				}
-				catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		}
-		jf.setSize(400,300);
-		jf.setLocation(Integer.parseInt(x),Integer.parseInt(y));
-		jf.setLayout(null);
-		JButton jb=new JButton("一键秒杀地方基地挂");
-		jb.setBounds(50,50,280,30);
-		jf.add(jb);
-		jf.setVisible(true);
-		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Object o=new Object();
-		Thread t=new Thread() {
-			public void run() {
-				synchronized(o) {
-					while(true) {
-						try {
-							o.wait(100);
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-						File f=new File("C:\\Data/location.txt");
-			//			FileOutputStream fos=new FileOutputStream(f);
-						try(FileWriter w= new FileWriter(f);) {
-							String x=String.valueOf(jf.getX());
-							String y=String.valueOf(jf.getY());
-							String temp=x+" "+y;
-							w.write(temp);
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}
-				}
-			}
-		};
-		t.setDaemon(true);
-		t.start();
+			};
+		b.addActionListener(l1);
 	}
 }
