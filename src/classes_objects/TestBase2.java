68ab44a342517f58fbb4c9bba67699a8df94f6e1
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2020-05-08 11:14:17 +0800

    接上一个commit。c++使用virtual虚函数后，再相同例子与java对比！
    		//但其实上述是基于，c++中，基类中的同名函数没有采用virtual虚函数
    		//只是重写！没有构成覆盖（c++中的重写、覆盖好像同java中不太一样）
    		//c++中重写与覆盖是两个概念
    		//java中重写等同于覆盖！
    		//倘若c++中，在基类的同名函数前面加上virtual构成虚函数
    		//就会跟java以上测试得到相同的结果了！

--- src/classes_objects/TestBase2.java
+++ src/classes_objects/TestBase2.java
@@ -15,6 +15,14 @@ public class TestBase2 extends TestBase{
 		//调用同名的成员函数时，有着不同处理方式！
 		//c++会使用具体的，被转化后的类里面的函数
 		//而java会使用具体被引用的那个对象的类里面的函数！
+		
+		//但其实上述是基于，c++中，基类中的同名函数没有采用virtual虚函数
+		//只是重写！没有构成覆盖（c++中的重写、覆盖好像同java中不太一样）
+		//c++中重写与覆盖是两个概念
+		//java中重写等同于覆盖！
+		//倘若c++中，在基类的同名函数前面加上virtual构成虚函数
+		//就会跟java以上测试得到相同的结果了！
+		
 	}
 	static public void test(TestBase t) {
 		t.print();
